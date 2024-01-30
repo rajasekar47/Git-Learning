3b5e93b0b24cbf5d4965953e17089160e4e02509
rajasekar47 <rajaarchi8316@gmail.com>
2024-01-30 22:49:41 +0530

    changes

--- script.js
+++ script.js
@@ -5,3 +5,17 @@
 var request = new XMLHttpRequest();
 //step2: open a request
 request.open("GET","https://restcountries.com/v3.1/all")
+//step3: kickstart a request
+request.send();
+//step4: once the data successfully received from the server
+//server status code is 200
+//data coming from the server is of type - string 
+//we are converting string to object.
+request.onload = function(){
+    var res = JSON.parse(request.response);
+    console.log(res);
+    //to print all 250 countries comman name:
+    for(var i=0;i<res.lenght;i++){
+    console.log(res[i].name.common)
+} 
+}
